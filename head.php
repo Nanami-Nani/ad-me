9d2daf0a9ef39f3891a740b2751fb2827f0a9947
Nanami-Nani <144137645+Nanami-Nani@users.noreply.github.com>
2025-01-08 21:29:35 +0700

    Add files via upload

---
+++ head.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html class="no-js" <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>"/>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<?php wp_head(); ?>
+	<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2639867148399078" crossorigin="anonymous"></script>
+</head>
