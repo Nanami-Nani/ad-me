ade6169b805cf09fc3871a06a5df5c998a3ab01c
Nanami-Nani <144137645+Nanami-Nani@users.noreply.github.com>
2025-01-14 19:43:01 +0700

    Add files via upload

---
+++ templates.php
@@ -0,0 +1,11 @@
+ <title><?PHP echo $rowBanner['meta_title'];?></title>
+        <meta name="description" content="<?PHP echo $rowBanner['meta_description'];?>">
+        <meta name="keywords" content="<?PHP echo $rowBanner['meta_keywords'];?>">
+        <meta charset="utf-8">
+        <meta name="author" content="AKA Yakiniku">
+        <meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1" />
+    <meta property="og:title" content="AKA Yakiniku"/>
+    <meta property="og:type" content="restaurant.restaurant"/>
+    <meta property="og:image" content="http://www.akayakiniku.com/images/aka/menu/banner.jpg"/>
+    <meta property="og:description" content="ร้านอาหารญี่ปุน AKA YAKINIKU ให้บริการ ทั้งแบบ A LA CART และ BUFFET โดยมีสาขามากกว่า 17 สาขา ในประเทศไทย ครอบคลุมพื้นที่กรุงเทพมหานครและเมืองหลักๆ ตามภูมิภาคต่างๆ ของประเทศ"/>
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9754282630311346" crossorigin="anonymous"></script>
