79da21101e53f6a034f3a27b92707a2dc189282c
Nanami-Nani <144137645+Nanami-Nani@users.noreply.github.com>
2025-01-24 19:13:12 +0700

    Create head1.php

---
+++ head1.php
@@ -0,0 +1,23 @@
+<!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-163386527-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-163386527-1');
+    </script>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Belize City Council</title>
+
+    <link rel="icon" href="<?=$root_url;?>favicon.ico" type="image/x-icon"/>
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9035780560703580" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="<?=$root_url;?>lib/css/uikit.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.12/summernote-lite.css">
+    <link type="text/css" rel="stylesheet" href="<?=$root_url;?>lib/css/jquery.dataTables.min.css"  media="screen,projection"/>
+    <link type="text/css" rel="stylesheet" href="<?=$root_url;?>lib/css/dataTables.uikit.min.css"  media="screen,projection"/>
+    <link type="text/css" rel="stylesheet" href="<?=$root_url;?>lib/css/flatpickr.min.css"  media="screen,projection"/>
+    <link rel="stylesheet" href="<?=$root_url;?>lib/css/master.css">
