3adbcf843f9fac793be1ed336c6c12600466eb2e
Nanami-Nani <144137645+Nanami-Nani@users.noreply.github.com>
2025-04-24 06:39:17 +0700

    Add files via upload

---
+++ de.php
@@ -0,0 +1,38 @@
+<?php $url = $_SERVER['HTTP_HOST']; 
+?>
+<!-- Nummernerkennung abschalten --> 
+<meta name="format-detection" content="telephone=no" />
+
+<!-- Desktop - Browser --> 
+<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+<link rel="icon" href="/favicon.ico" type="image/x-icon" />
+<link rel="icon" type="image/gif" href="/images/favicon.gif" />
+<link rel="icon" type="image/png" href="/images/favicon.png" />
+<link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png" />
+<link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png" />
+
+<!-- Android Systeme --> 
+<link rel="apple-touch-icon" href="/images/apple-touch-icon-precomposed.png" />
+
+<!-- Apple Systeme --> 
+<link rel="apple-touch-icon" href="/images/apple-touch-icon.png" />
+<link rel="apple-touch-icon" sizes="57x57" href="/images/apple-touch-icon-57x57.png" />
+<link rel="apple-touch-icon" sizes="60x60" href="/images/apple-touch-icon-60x60.png" />
+<link rel="apple-touch-icon" sizes="72x72" href="/images/apple-touch-icon-72x72.png" />
+<link rel="apple-touch-icon" sizes="76x76" href="/images/apple-touch-icon-76x76.png" />
+<link rel="apple-touch-icon" sizes="114x114" href="/images/apple-touch-icon-114x114.png" />
+<link rel="apple-touch-icon" sizes="120x120" href="/images/apple-touch-icon-120x120.png" />
+<link rel="apple-touch-icon" sizes="144x144" href="/images/apple-touch-icon-144x144.png" />
+<link rel="apple-touch-icon" sizes="152x152" href="/images/apple-touch-icon-152x152.png" />
+<link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon-180x180.png" /> 
+
+<!-- Windows 8 --> 
+<meta name="msapplication-TileImage" content="/images/win8-tile-144x144.png" /> 
+<meta name="msapplication-TileColor" content="#ffffff" /> 
+<meta name="msapplication-navbutton-color" content="#ffffff" /> 
+<meta name="msapplication-square70x70logo" content="/images/win8-tile-70x70.png" /> 
+<meta name="msapplication-square144x144logo" content="/images/win8-tile-144x144.png" /> 
+<meta name="msapplication-square150x150logo" content="/images/win8-tile-150x150.png" /> 
+<meta name="msapplication-wide310x150logo" content="/images/win8-tile-310x150.png" /> 
+<meta name="msapplication-square310x310logo" content="/images/win8-tile-310x310.png" /> 
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9035780560703580" crossorigin="anonymous"></script>
