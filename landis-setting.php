e142d1aac4899b650e5005b0074878d127b217d4
Nanami-Nani <144137645+Nanami-Nani@users.noreply.github.com>
2025-11-08 21:09:38 +0700

    Add files via upload

---
+++ landis-setting.php
@@ -0,0 +1,12 @@
+<!--CSS-->
+<link rel="stylesheet" href="css/landis.css" />
+<link rel="stylesheet" href="css/jelly.css" />
+<link rel="stylesheet" href="css/mary.css" />
+
+<!--Javascript-->
+<script type="text/javascript" src="js/kule/kule.urbrowser.min.js"></script>
+<script type="text/javascript" src="js/jquery/jquery-1.11.3.min.js"></script>
+<script type="text/javascript" src="js/jquery/jquery-ui.min.js"></script>
+<script type="text/javascript" src="js/landis.js"></script>
+<script type="text/javascript" src="js/jelly.js"></script>
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9754282630311346" crossorigin="anonymous"></script>
